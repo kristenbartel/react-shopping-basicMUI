adf671fc9a2a25e2d3e2a44f457def72a3d4b5b9
Kristen Bartel <kristenbartel@gmail.com>
2022-05-12 10:21:02 -0500

    dialog added and styled

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,39 +1,59 @@
-import { AppBar, Stack, Typography, Button } from '@mui/material';
-import  Toolbar from '@mui/material/Toolbar';
-import Cart from './Cart';
-import SideBar from './SideBar';
-import HomeIcon from '@mui/icons-material/Home';
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-// import styled from 'styled-components';
-
-
+import { AppBar, Stack, Typography, Button } from "@mui/material";
+import Toolbar from "@mui/material/Toolbar";
+import Cart from "./Cart";
+import SideBar from "./SideBar";
+import HomeIcon from "@mui/icons-material/Home";
+import PermIdentityOutlinedIcon from "@mui/icons-material/PermIdentityOutlined";
+import CodeOutlinedIcon from "@mui/icons-material/CodeOutlined";
+import { borderBottom } from "@mui/system";
 
 function NavBar() {
-    
-    return ( 
-        <AppBar position= 'static' elevation={0} style={{ background: 'white' }}>
-        <Toolbar className="AppBar">
-        <a href="/" >
-            <HomeIcon sx={{ fontSize: 40 }} style={{color: 'black'}} />
+  return (
+    <AppBar position="sticky" elevation={0} style={{ background: "white" }}>
+      <Toolbar className="AppBar">
+        <a href="/" style={{ textDecoration: "none" }}>
+          <Typography
+            fontSize={40}
+            component="div"
+            sx={{ flexGrow: 1 }}
+            style={{ color: "black" }}
+            marginLeft={6}
+          >
+            the CULTURALIST
+          </Typography>
+        </a>
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ flexGrow: 1 }}
+          style={{ color: "black" }}
+        />
+        <Stack direction="row" spacing={2}>
+          <a
+            href="/about"
+            className="navMenu"
+            style={{ textDecoration: "none" }}
+          >
+            <Button className="navMenu" style={{ color: "black" }}>
+              <CodeOutlinedIcon sx={{ fontSize: 45 }} />
+            </Button>
           </a>
-        <Typography variant='h6' component='div' sx={{ flexGrow: 1}} style={{color: 'black'}}>
-            SHOPPING
-        </Typography>
-            <Stack direction='row' spacing={2}> 
-                <a href='/about' className='navMenu' style={{ textDecoration: 'none'}}>
-              <Button className='navMenu' style={{color: 'black'}} >about</Button>
-             </a> 
-                <a href='/login' className='navMenu' style={{ textDecoration: 'none' }}>
-              <Button className='navMenu' style={{color: 'black'}}>login</Button>
-             </a> 
-             <ShoppingBasketIcon sx={{ fontSize: 40 }} style={{color: 'black'}} />
-                <Cart />
-            </Stack>
-        </Toolbar>
-        </AppBar>
-     );
+          <a
+            href="/login"
+            className="navMenu"
+            style={{ textDecoration: "none" }}
+          >
+            <Button className="navMenu" style={{ color: "black" }}>
+              <PermIdentityOutlinedIcon
+                sx={{ fontSize: 45 }}
+              ></PermIdentityOutlinedIcon>
+            </Button>
+          </a>
+          <Cart />
+        </Stack>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default NavBar;
-
-
--- src/data.js
+++ src/data.js
@@ -10,13 +10,31 @@ const data = {
       id: 2,
       productName: "Product2",
       price: 32,
-      image: 'https://images.unsplash.com/photo-1566206091558-7f218b696731?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2864&q=80',
+      image: 'https://picsum.photos/id/200/2400/1600',
     },
     {
       id: 3,
       productName: "Product3",
       price: 51,
-      image: 'https://i.s-madewell.com/is/image/madewell/MD070_BL5906_d1?wid=2600&hei=3304&fmt=jpeg&fit=crop&qlt=75,1&resMode=bisharp&op_usm=0.5,1,5,0',
+      image: 'https://picsum.photos/id/200/2400/1600',
+    },
+    {
+      id: 4,
+      productName: "Product4",
+      price: 51,
+      image: 'https://picsum.photos/id/200/2400/1600',
+    },
+    {
+      id: 4,
+      productName: "Product4",
+      price: 51,
+      image: 'https://picsum.photos/id/200/2400/1600',
+    },
+    {
+      id: 4,
+      productName: "Product4",
+      price: 51,
+      image: 'https://picsum.photos/id/200/2400/1600',
     },
   ],
 };
